eda8938e2b882dbec06810219a1100bb688df6c0
Siim Koger <koger@rushstreetinteractive.com>
2019-06-04 16:08:52 +0300

    Finished xml downloader (didn't do the last video)

--- app/src/main/java/com/example/kotlin_android_tut/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_android_tut/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.kotlin_android_tut
 import android.content.Context
 import android.os.AsyncTask
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
 import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,8 +16,13 @@ import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private val downloadData by lazy { DownloadDataAsyncTask(this, xmlListView) }
-    private var rssUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/limit=200/xml"
+    private var downloadData: DownloadDataAsyncTask? = null
+    private var feedLimit = 10
+    private val feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/%s"
+    private val moviesUrl = feedUrl.format("topMovies/limit=%d/xml")
+    private val tvSeasonsUrl = feedUrl.format("topTvSeasons/limit=%d/xml")
+    private val tvEpisodesUrl = feedUrl.format("topTvEpisodes/limit=%d/xml")
+    private var currentUrl = moviesUrl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate called")
@@ -27,29 +30,54 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        downloadData.execute(rssUrl)
+        downloadFeed(moviesUrl, 10)
         Log.d(TAG, "onCreate finish")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        downloadData.cancel(true)
+        downloadData?.cancel(true)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.feeds_menu, menu)
+
+        if (feedLimit == 10) {
+            menu.findItem(R.id.menuLimitTop10)?.isChecked = true
+        } else {
+            menu.findItem(R.id.menuLimitTop25)?.isChecked = true
+        }
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
+        when (item.itemId) {
+            R.id.menuMovies -> currentUrl = moviesUrl
+            R.id.menuTvSeasons -> currentUrl = tvSeasonsUrl
+            R.id.menuTvEpisodes -> currentUrl = tvEpisodesUrl
+            R.id.menuLimitTop10 -> {
+                if (!item.isChecked) {
+                    item.isChecked = true
+                    feedLimit = 10
+                }
+            }
+            R.id.menuLimitTop25 -> {
+                if (!item.isChecked) {
+                    item.isChecked = true
+                    feedLimit = 25
+                }
+            }
             else -> super.onOptionsItemSelected(item)
         }
+        downloadFeed(currentUrl, feedLimit)
+        return true
+    }
+
+    private fun downloadFeed(feedUrl: String, limit: Int) {
+        currentUrl = feedUrl
+        downloadData = DownloadDataAsyncTask(this, xmlListView)
+        downloadData?.execute(feedUrl.format(limit))
     }
 
     companion object {
